1d1c919c608b8cdfef1245c11028d1927f96c9f0
Madrid-2003 <www.madridmandal.in@gmail.com>
2025-08-28 01:47:14 +0530

    forteen

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api': 'https://learning-plat-36hz.vercel.app/',
       '/payu-payment': {
         target: 'https://test.payu.in/_payment',
         changeOrigin: true,
