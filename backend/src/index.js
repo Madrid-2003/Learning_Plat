5b342347422eb8624c2f0affe7241af152d26c0d
Madrid-2003 <www.madridmandal.in@gmail.com>
2025-08-28 01:05:23 +0530

    eight

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,40 +1,21 @@
-// import dotenv from "dotenv"
-// import db from './database/db.js';
-// import {app} from './app.js'
-// dotenv.config({
-//     path: './.env'
-// })
-
-// console.log(`${process.env.DB_NAME}`);
-// app.get("/",(req,res)=>{
-//     res.send("Welcome to the e-Learning Platform");
-// })
-
-// db()
-// .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-//     })
-// })
-// .catch((err) => {
-//     console.log(" mongodb connection failed !!! ", err);
-// })
-// src/index.js
 import dotenv from "dotenv"
 import db from './database/db.js';
-import { app } from './app.js'
-
+import {app} from './app.js'
 dotenv.config({
     path: './.env'
 })
 
-// Establish database connection immediately when the function starts
-// This is a good practice for Vercel's serverless environment
-db();
-
-app.get("/", (req, res) => {
+console.log(`${process.env.DB_NAME}`);
+app.get("/",(req,res)=>{
     res.send("Welcome to the e-Learning Platform");
 })
 
-// Export the Express app instance directly for Vercel
-export default app;
+db()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log(" mongodb connection failed !!! ", err);
+})
