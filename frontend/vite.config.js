170e5a003726988ce3dc671aeb1818a275a09b37
Madrid-2003 <www.madridmandal.in@gmail.com>
2025-08-28 01:24:31 +0530

    twelve:front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api': 'http://localhost:4400',
+      '/api': 'https://learning-plat-36hz.vercel.app/',
       '/payu-payment': {
         target: 'https://test.payu.in/_payment',
         changeOrigin: true,
